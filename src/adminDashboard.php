4ccd0eb0a441a887f221042aa07ea7a7acce7463
eabde <elmellalou@gmail.com>
2024-05-15 21:54:18 +0200

    Aggiunto layout e funzionalità alla pagina Admin

--- src/adminDashboard.php
+++ src/adminDashboard.php
@@ -1,10 +1,39 @@
 <html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
-</head>
-<body>
-    
-</body>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/dashboard.css">
+        <title>Admin</title>
+    </head>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <b>Admin</b>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="btn btn-primary" href="logout.php">Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <body>
+        <div class="btn-container">
+        <button type="button" class="btn btn-warning">Aggiungi stazione</button>
+            <button type="button" class="btn btn-warning">Aggiungi bicicletta</button>
+            <button type="button" class="btn btn-warning">Modifica informazioni biciclette</button>
+            <button type="button" class="btn btn-warning">Modifica informazioni stazioni</button>
+            <button type="button" class="btn btn-warning">Visualizza mappa</button>
+        </div>
+    </body>
+
 </html>
